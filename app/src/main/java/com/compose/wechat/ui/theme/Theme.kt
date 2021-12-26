5ef83f7dc3db7a07563acc6320603e8ab4216e65
a284628487 <284628487@qq.com>
2021-12-26 16:40:46 +0800

    [ccf]add composition local

--- app/src/main/java/com/compose/wechat/ui/theme/Theme.kt
+++ app/src/main/java/com/compose/wechat/ui/theme/Theme.kt
@@ -5,6 +5,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.compose.wechat.R
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -27,18 +31,36 @@ private val LightColorPalette = lightColors(
     */
 )
 
-@Composable
-fun WeChatTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+open class AbsThemeMode(var backgroundResId: Int, var logoResId: Int)
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+object LightThemeMode : AbsThemeMode(R.drawable.ic_launcher_background, R.drawable.ic_frag)
+
+object DarkThemeMode : AbsThemeMode(R.drawable.ic_launcher_background, R.drawable.ic_frag)
+
+internal var LocalThemeMode = staticCompositionLocalOf<AbsThemeMode> { LightThemeMode }
+
+val MaterialTheme.themeMode
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalThemeMode.current
+
+@Composable
+fun WeChatTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val welcomeAssets = if (darkTheme) DarkThemeMode else LightThemeMode
+    CompositionLocalProvider(
+        LocalThemeMode provides welcomeAssets,
+    ) {
+        val colors = if (darkTheme) {
+            DarkColorPalette
+        } else {
+            LightColorPalette
+        }
+
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
